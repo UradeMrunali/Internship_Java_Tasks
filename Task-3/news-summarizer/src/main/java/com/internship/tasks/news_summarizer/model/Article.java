04c964f47f34f5639e98f82c9261536a5834dcd3
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2026-01-11 21:35:43 +0530

    Java-week3-task

--- Task-3/news-summarizer/src/main/java/com/internship/tasks/news_summarizer/model/Article.java
+++ Task-3/news-summarizer/src/main/java/com/internship/tasks/news_summarizer/model/Article.java
@@ -2,11 +2,11 @@ package com.internship.tasks.news_summarizer.model;
 
 public class Article {
     private String title;
-    private String summary; // This is the AI-summarized text
+    private String summary; 
     private String originalUrl;
     private String imageUrl;
 
-    // Constructor
+  
     public Article(String title, String summary, String originalUrl, String imageUrl) {
         this.title = title;
         this.summary = summary;
@@ -14,7 +14,7 @@ public class Article {
         this.imageUrl = imageUrl;
     }
 
-    // Getters and Setters
+    
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
 
